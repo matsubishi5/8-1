229a3e17858e3c8568e9e51e073385e902edbbe2
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-04-05 18:20:41 +0900

    [Finish]8-1

--- bookers2-debug/app/controllers/application_controller.rb
+++ bookers2-debug/app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
+    ThanksMailer.send_confirm_to_user(current_user).deliver
     user_path(current_user)
   end
 
